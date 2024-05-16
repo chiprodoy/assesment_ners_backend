f07c926162b294dccde2d7ed61feaa160f65c245
chiprodoy <chiprodoy@gmail.com>
2024-05-17 06:28:09 +0700

    perbaikan pada report asesmen

--- resources/views/pdf/personal_report.blade copy.php
+++ resources/views/pdf/personal_report.blade copy.php
@@ -116,6 +116,66 @@ Nama : {{$mahasiswa->nama}}<br/>
 
 </table>
 <h2>KUMULATIF NILAI </h2>
+<table width='80%' border="1" cellspacing=0 cellpadding=3>
+    <tr>
+        <td rowspan="2">No</td>
+        <td rowspan="2">Asesmen</td>
+        <td colspan="4">Kompetensi</td>
+        <td rowspan="2">Total Nilai</td>
+        <td rowspan="2">Grade</td>
+    </tr>
+    <tr>
+        <td>Kognitif</td>
+        <td>Psikomotor</td>
+        <td>Afektif</td>
+        <td>Sosial</td>
+    </tr>
+    <tr>
+        <td></td>
+        <td>{{$nilaiSubKompetensi[0]->sub_kompetensi->kompetensi->asesmen->nama_asesmen}}</td>
+        <td>{{$subTotals[0]}}</td>
+        <td>{{$subTotals[1]}}</td>
+        <td>{{$subTotals[2]}}</td>
+        <td>{{$subTotals[3]}}</td>
+        <td>{{$subTotals[0] + $subTotals[1] + $subTotals[2] + $subTotals[3]}}</td>
+        <td></td>
+    </tr>
+    @php
+        $naKognitif = $naKognitif+$subTotals[0];
+        $naPsikoMotor = $naPsikoMotor+$subTotals[1];
+        $naAfektif = $naAfektif+$subTotals[2];
+        $naSocial = $naSocial+$subTotals[3];
 
+    @endphp
+    <tr>
+        <td></td>
+        <td>Nilai Akhir</td>
+        <td>{{$naKognitif}}</td>
+        <td>{{$naPsikoMotor}}</td>
+        <td>{{$naAfektif}}</td>
+        <td>{{$naSocial}}</td>
+        <td>{{$totalNA=$naKognitif + $naPsikoMotor + $naAfektif + $naSocial}}</td>
+        <td>
+            @php
+                if($totalNA <= 100 && $totalNA >= 80){
+                    $grade='A';
+                }elseif ($totalNA < 80 && $totalNA >= 70) {
+                    # code...
+                    $grade='B';
+
+                }elseif ($totalNA < 70 && $totalNA >= 51) {
+                    # code...
+                    $grade='C';
+                }elseif ($totalNA < 51 && $totalNA >= 30) {
+                    # code...
+                    $grade='D';
+                }else{
+                    $grade='E';
+                }
+            @endphp
+            {{$grade}}
+        </td>
+    </tr>
+</table>
 </body>
 </html>
